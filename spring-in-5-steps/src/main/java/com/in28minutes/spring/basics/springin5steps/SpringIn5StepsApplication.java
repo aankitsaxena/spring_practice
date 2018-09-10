0e551dadde04d4be602e4cfe5c2c6b07385de053
aankitsaxena <aankitsaxena.10@gmail.com>
2018-09-10 20:42:22 +0530

    Added annotation

--- spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsApplication.java
+++ spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsApplication.java
@@ -2,16 +2,18 @@ package com.in28minutes.spring.basics.springin5steps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringIn5StepsApplication {
 
 	public static void main(String[] args) {
-		BinarySearchImpl binarySearch = new BinarySearchImpl(new QuickSortAlgorithm());
+		//BinarySearchImpl binarySearch = new BinarySearchImpl(new QuickSortAlgorithm());
+		
+		ApplicationContext context = SpringApplication.run(SpringIn5StepsApplication.class, args);
+		BinarySearchImpl binarySearch = context.getBean(BinarySearchImpl.class);
 		int result = binarySearch.binarySearch(new int[] {1,2,3,4,5}, 3);
 		System.out.println(result);
-		
-		//SpringApplication.run(SpringIn5StepsApplication.class, args);
 		
 		
 	}
