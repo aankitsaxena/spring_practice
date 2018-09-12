5499068c4228f240a2d85d1721bb60fbc082d94a
aankitsaxena <aankitsaxena.10@gmail.com>
2018-09-12 17:06:33 +0530

    Scope (Singleton and Prototype) and proxyMode thing

--- spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsBasicApplication.java
+++ spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsBasicApplication.java
@@ -4,13 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import com.in28minutes.spring.basics.springin5steps.basic.BinarySearchImpl;
+
 @SpringBootApplication
-public class SpringIn5StepsApplication {
+public class SpringIn5StepsBasicApplication {
 
 	public static void main(String[] args) {
 		//BinarySearchImpl binarySearch = new BinarySearchImpl(new QuickSortAlgorithm());
 		
-		ApplicationContext context = SpringApplication.run(SpringIn5StepsApplication.class, args);
+		ApplicationContext context = SpringApplication.run(SpringIn5StepsBasicApplication.class, args);
 		BinarySearchImpl binarySearch = context.getBean(BinarySearchImpl.class);
 		
 		BinarySearchImpl binarySearch1 = context.getBean(BinarySearchImpl.class);
