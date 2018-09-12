7268415a67d4467274a7412ff9503462e5dd93fd
aankitsaxena <aankitsaxena.10@gmail.com>
2018-09-12 18:16:23 +0530

    @ComponentScan annotation

---
+++ spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/componentScan/ComponentDAO.java
@@ -0,0 +1,24 @@
+package com.in28minutes.spring.basics.componentScan;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ComponentDAO {
+	
+	@Autowired
+	ComponentJdbcConnection componentJdbcConnection;
+
+	public ComponentJdbcConnection getComponentJdbcConnection() {
+		return componentJdbcConnection;
+	}
+
+	public void setComponentJdbcConnection(ComponentJdbcConnection componentJdbcConnection) {
+		this.componentJdbcConnection = componentJdbcConnection;
+	}
+
+	
+	
+}
