0b59ec75bb650fa16dd9b0dd539510a9249fd543
aankitsaxena <aankitsaxena.10@gmail.com>
2018-09-12 21:46:58 +0530

    ..

--- spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/basic/BinarySearchImpl.java
+++ spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/basic/BinarySearchImpl.java
@@ -1,5 +1,10 @@
 package com.in28minutes.spring.basics.springin5steps.basic;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
@@ -13,34 +18,28 @@ public class BinarySearchImpl {
 	@Autowired
 	@Qualifier("bubble")
 	private SortAlgorithm sortAlgorithm;
-
-
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	public BinarySearchImpl(SortAlgorithm bubbleSortAlgorithm) {
 		super();
 		this.sortAlgorithm = bubbleSortAlgorithm;
 	}
 
-
-
 	public int binarySearch(int[] numbers, int number) {
-		
-		
 		int[] sortedNumbers = sortAlgorithm.sort(numbers);
 		System.out.println(sortAlgorithm);
 		
 		return 3;
 	}
-
-
-
-	public SortAlgorithm getBubbleSortAlgorithm() {
-		return sortAlgorithm;
+	
+	@PostConstruct
+	public void postConstruct() {
+		logger.info("Post Construct****");
 	}
 
-
-
-	public void setBubbleSortAlgorithm(SortAlgorithm bubbleSortAlgorithm) {
-		this.sortAlgorithm = bubbleSortAlgorithm;
+	@PreDestroy
+	public void preDestroy() {
+		logger.info("Pre Destroy****");
 	}
 }
